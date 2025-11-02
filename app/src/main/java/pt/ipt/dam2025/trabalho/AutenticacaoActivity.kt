9ac5f59bf346474bf57a92a6c133d5d98ef8efab
const <constancaazevedo3@gmail.com>
2025-11-02 13:46:00 +0000

    Versao 14 - criação de um campo para pedir número de telemóvel

--- app/src/main/java/pt/ipt/dam2025/trabalho/AutenticacaoActivity.kt
+++ app/src/main/java/pt/ipt/dam2025/trabalho/AutenticacaoActivity.kt
@@ -3,6 +3,7 @@ package pt.ipt.dam2025.trabalho
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,11 +12,16 @@ class AutenticacaoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_autenticacao)
 
+        val phoneNumberInput = findViewById<EditText>(R.id.phone_number_input)
         val loginButton = findViewById<Button>(R.id.login_button2)
+        
         loginButton.setOnClickListener {
+            // 1. Obter o número de telemóvel
+            val phoneNumber = phoneNumberInput.text.toString()
+
+            // 2. Ir para a HomeActivity
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
     }
-
 }
