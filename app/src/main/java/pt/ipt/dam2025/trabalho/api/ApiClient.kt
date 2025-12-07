3c7aeca2c977087be4478a528cf05d82af0cd7d3
const <constancaazevedo3@gmail.com>
2025-12-07 01:04:57 +0000

    versao 55 - configuração da API publicamente, e começar a comentar o código todo

--- app/src/main/java/pt/ipt/dam2025/trabalho/api/ApiClient.kt
+++ app/src/main/java/pt/ipt/dam2025/trabalho/api/ApiClient.kt
@@ -7,30 +7,43 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
+/**
+ * configura a conexão da aplicação com o servidor
+ */
+
+
+//Criar um Singleton -> só exisitirá uma única instância de ApiClient em toda a aplicação
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/" // Para emulador
+    private const val BASE_URL = "https://vetconnect-api-production.up.railway.app" //URL da API
 
-    // Para dispositivo físico (mesma rede WiFi):
-    // private const val BASE_URL = "http://192.168.x.x:3000/"
 
+    //Criar um intercetor de loggin - regista os detalhes das chamadas de rede
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
+        level = HttpLoggingInterceptor.Level.BODY //Regista o máximo de informação possível
     }
 
+
+    //Cria o cliente HTTP com os interceptores
     private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(loggingInterceptor)
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .writeTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(loggingInterceptor) //Adiciona o interceptor de loggin ao cliente - todas as chamadas serão registadas no Logcat
+        .connectTimeout(30, TimeUnit.SECONDS) //Tempo limite para conexção com o servidor
+        .readTimeout(30, TimeUnit.SECONDS) //Tempo limite para receber resposta do servidor
+        .writeTimeout(30, TimeUnit.SECONDS) //Tempo limite para enviar dados ao servidor
         .build()
 
+
+    //Cria o objeto principal do Retrofit
     val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .client(okHttpClient)
-        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL) //Endereço base da API
+        .client(okHttpClient) //Cliente HTTP com os interceptores
+        .addConverterFactory(GsonConverterFactory.create()) //Converte o JSON para objetos Kotlin usando a biblioteca Gson
         .build()
 
+
+    //Cria a instância do serviço da API
+    // by lazy significa que este código só é executado na primeira vez que o ApiService é usado
+    // nas vezes seguintes a instância é reutilizada
     val apiService: ApiService by lazy {
-        retrofit.create(ApiService::class.java)
+        retrofit.create(ApiService::class.java) //Cria uma instância do serviço da API usando o objeto Retrofit
     }
 }
