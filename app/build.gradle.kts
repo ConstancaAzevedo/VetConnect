3ffea762405b6c12a6d9f54469a900245b07523e
const <constancaazevedo3@gmail.com>
2026-01-28 17:04:59 +0000

    versao 70 - implementação da camara com o codigo do professor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    //chave que vai ativar o binding
+    //copiado do outro ficheiro "code-Camera X app.vf"
+    // allow the access to objects of the interface, from code,
+    // in 'binding'
+    buildFeatures {
+        viewBinding=true
+    }
 }
 
 dependencies {
@@ -44,9 +52,32 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     // Retrofit & Gson -> para a API
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.google.code.gson:gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
+    implementation(libs.logging.interceptor)
+
+    // código do professor para Camera
+    //copiado do outro ficheiro "code-Camera X app.vf"
+    // CameraX core library using the camera2 implementation
+    val cameraxVersion = "1.5.1"
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    // implementation("androidx.camera:camera-core:${cameraxVersion}")
+    implementation(libs.androidx.camera.core)
+    // implementation("androidx.camera:camera-camera2:${cameraxVersion}")
+    implementation(libs.androidx.camera.camera2)
+    // If you want to additionally use the CameraX Lifecycle library
+    // implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
+    implementation(libs.androidx.camera.lifecycle)
+    // If you want to additionally use the CameraX VideoCapture library
+    //    implementation "androidx.camera:camera-video:${camerax_version}"
+    // If you want to additionally use the CameraX View class
+    // implementation("androidx.camera:camera-view:${cameraxVersion}")
+    implementation(libs.androidx.camera.view)
+    // If you want to additionally add CameraX ML Kit Vision Integration
+    //       implementation "androidx.camera:camera-mlkit-vision:${camerax_version}"
+    // If you want to additionally use the CameraX Extensions library
+    // implementation("androidx.camera:camera-extensions:${cameraxVersion}")
+    implementation(libs.androidx.camera.extensions)
 
 }
